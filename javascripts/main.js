f5c12c082ae3936d0a344f2603df222bc99492de
J2TeaM <juno_okyo@yahoo.com.vn>
2015-08-30 13:39:00 +0700

    Added main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-// console.log('This would be the main JS file.');
+var host = window.top.location.host,
+	urls = document.links,
+	total = urls.length;
+	
+for (var i = 0; i < total; i++) {
+	var link = urls[i];
+	
+	if (link.href.indexOf(host) === -1)
+		link.setAttribute('target', '_blank');
+}
